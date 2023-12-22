3e8b3192cd67172a8db93d6b54a1ab32fe2675a1
Chenzorama <chenzorama@hotmail.com>
2023-12-22 08:23:13 -0500

    first commit

---
+++ app/VoiceToText.js
@@ -0,0 +1,28 @@
+
+
+
+//import styles from "./TollButton.module.scss";
+
+import { useState, useEffect } from 'react'
+
+export default function VoiceToText() {
+
+
+    let SpeechRecognition = null;
+    let SpeechGrammarList = null;
+    let SpeechRecognitionEvent = null;
+  
+    useEffect(() => {
+      console.log("Should be good");
+      SpeechRecognition = window.webkitSpeechRecognition
+      SpeechGrammarList = window.webkitSpeechGrammarList
+      SpeechRecognitionEvent = window.webkitSpeechRecognitionEvent
+    }, []); 
+
+    
+  return (
+    <div>
+        <p class="output"><em>…diagnostic messages</em></p>
+      </div>
+  );
+}
